9a89da82f0d96816ce22133e295130b31cad5086
Fredy Colorado <yomacolor42@gmail.com>
2022-01-28 21:00:13 -0500

    28 enero 2022

--- js/Math_random.js
+++ js/Math_random.js
@@ -1,8 +1,9 @@
-// CARPETA METODOS
+// CARPETA MÉTODOS
 
 // Escribe un programa que piense un número de forma aleatoria del 1 al 10 y le pida al usuario que lo trate de adivinar. Si el número es correcto debe imprimir en la consola "Felicitaciones, ese era!", de lo contrario debe imprimir "Lo siento, intenta nuevamente!"
 
 
+// número de forma aleatoria del 1 al 10
 let escogerNumero = Math.floor((Math.random() * 10) + 1);
 console.log(escogerNumero);
 
